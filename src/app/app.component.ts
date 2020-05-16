8d51b24a888bcda6e31c85e6ffd98dd361869663
Badr Matrane <matrane.badr@gmail.com>
2020-05-17 01:33:44 +0200

    first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
-  
+import { Component,OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pockemons';
+
 @Component({
   selector: 'pokemon-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Liste de pokémons</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit { 
+  
+  private pokemons: Pokemon[]; 
+
+  ngOnInit()
+  {
+    this.pokemons=POKEMONS;
+  }
+  selectPokemon(pokemon: Pokemon)
+  {
+    alert("Vous avez cliqué sur" +pokemon.name)
+  }
+}
