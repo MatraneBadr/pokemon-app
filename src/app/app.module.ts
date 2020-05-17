9be309dc5bee5ba9b39c2cbffc812d4730a9bcad
Badr Matrane <matrane.badr@gmail.com>
2020-05-17 05:56:13 +0200

    pipe personalisé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
   
 import { AppComponent }  from './app.component';
 import { BorderCardDirective } from './border-card.directive' 
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent,
-                  BorderCardDirective],
+                  BorderCardDirective,
+                PokemonTypeColorPipe],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
